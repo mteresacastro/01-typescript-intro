bd960b1b6dfb298e7c817cba643cc8bd529d1670
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-05-06 13:38:06 +0200

    tipado de objetos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types.ts'
+import './topics/02-object-inferface.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  hola mundo
---
+++ src/topics/02-object-inferface.ts
@@ -0,0 +1,23 @@
+
+let skills: string[] = ['Bash', 'Counter', 'Healing'];
+
+//tipado de objetos: Se realiza mediante interfaz
+
+interface Character{
+    name: string;
+    hp: number;
+    skills: string[];
+    hometown?: string; //se pone la interrogacion tras la propiedad para que sea opcional
+}
+
+const Yin: Character = {
+    name: 'Yin',
+    hp: 100,
+    skills: ['Bash', 'Counter', 'Healing'],
+    hometown: 'Rivendell'
+
+}
+
+console.table(Yin)
+
+export{};
