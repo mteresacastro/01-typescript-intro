a050660df70c185e9046ad09f5208ae850694404
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-05-17 11:44:00 +0200

    classes2

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,18 +1,26 @@
 
 export class Person{
 
-    public name: string;
-    public address: string;
+    // public name: string;
+    // public address: string;
 
-    constructor(){
-        this.name = "Tere";
-        this.address = "Sevilla";
+    //public o private lo podemos poner diractamente en el constructor
+
+    //si le doy datos fijos al constructor, siempre que cree una instancia se volcarán esos datos.
+    // constructor(){
+    //     this.name = "Tere";
+    //     this.address = "Sevilla";
+    // } por ello lo haremos de la siguiente manera:
+
+    constructor(public name: string, private address: string){
+        this.name = name;
+        this.address = address;
     }
 
 }
 
 
-const ironMan = new Person()
+const ironMan = new Person("Jonh", "Madrid");
 
 
 console.log(ironMan);
